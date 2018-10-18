6a5738795374ab26933962cfec6334959d44a9fa
Unknown <anthony.marechal@yahoo.fr>
2018-10-18 08:51:13 +0200

    :construction: Reading console arguments
    First function, read arguments and display it in console

--- 154.c
+++ 154.c
@@ -1,9 +1,33 @@
 //
-// Created by MARECHAL on 16/10/2018.
+// Created by MARECHAL Anthony & MOZDZIERZ Ombeline on 16/10/2018.
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct s{
+    int valid;
+    double tag;
+}bloc;
+
+/*typedef bloc Cache [][];
+
+void initialise(int nbe, int assoc, Cache cache) {
+    int i, j;
+    for (i=0; i<nbe; i++)
+        for (j=0; j<assoc; j++)
+            cache[i][j].valid=0;
+}*/
 
 int main(int argc, char* argv[]){
-    printf("Hello world !");
+int cs, bs, assoc;
+char* tmp, trace;
+
+cs = strtol(argv[1], &tmp, 10);
+bs = strtol(argv[2], &tmp, 10);
+assoc = strtol(argv[3], &tmp, 10);
+strcpy(&trace, argv[4]);
+
+printf("%d, %d, %d, %s", cs, bs, assoc, &trace);
 }
