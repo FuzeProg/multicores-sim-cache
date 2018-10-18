44ec24d265e92a6d62209b1ccdb1f5a8c492d96e
Unknown <anthony.marechal@yahoo.fr>
2018-10-18 11:49:54 +0200

    :construction: Args check
    Checking corrects arguments

--- 154.c
+++ 154.c
@@ -5,11 +5,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
-typedef struct s{
+typedef struct s {
     int valid;
     double tag;
-}bloc;
+} bloc;
 
 /*typedef bloc Cache [][];
 
@@ -20,17 +21,43 @@ void initialise(int nbe, int assoc, Cache cache) {
             cache[i][j].valid=0;
 }*/
 
-int main(int argc, char* argv[]){
+void isCorrect(int argcs, char *argvs[]) {
+    if (argcs < 5 || argcs > 5) {
 
-long cs, bs, assoc;
-char* tmp;
-char* trace;
+        printf("Arguments manquants : \n"
+               "1 : programme 154.c. \n"
+               "2 : taille du cache. Vous avez saisi : %s\n"
+               "3 : taille du bloc. Vous avez saisi : %s\n"
+               "4 : degrés d'associativité. Vous avez saisi : %s\n"
+               "5 : fichier à parcourir. Vous avez saisi : %s\n",
+               argvs[1], argvs[2], argvs[3], argvs[4]);
+        exit(1);
+    }
+}
 
-cs = strtol(argv[1], &tmp, 10);
-bs = strtol(argv[2], &tmp, 10);
-assoc = strtol(argv[3], &tmp, 10);
-trace = argv[4];
+void test(char *argvs[], int c){
+    if(isnumber((int)strtol(argvs[c], NULL, 10)))
+        puts("C'est un chiffre");
+    else
+        puts("C'est un texte");
+}
 
-printf("%li, %li, %li, %s", cs, bs, assoc, trace);
+int main(int argc, char *argv[]) {
+
+    long cs, bs, assoc;
+    char *tmp, *trace;
+
+    isCorrect(argc, argv);
+    test(argv, 1);
+    test(argv, 4);
+    printf("%li", strtol(argv[4], &tmp, 10));
+    exit(1);
+
+    cs = strtol(argv[1], &tmp, 10);
+    bs = strtol(argv[2], &tmp, 10);
+    assoc = strtol(argv[3], &tmp, 10);
+    trace = argv[4];
+
+    printf("%li, %li, %li, %s", cs, bs, assoc, trace);
 
 }
