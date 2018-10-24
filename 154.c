b823e43336920601950b9feba4c03f0e34b3cfcb
Unknown <anthony.marechal@yahoo.fr>
2018-10-24 08:08:59 +0200

    :construction: Split line into char | adr

--- 154.c
+++ 154.c
@@ -19,33 +19,43 @@ void initCache(long nbe, long assoc, bloc cache[nbe][assoc]){
 }
 
 // Print out cache configuration
-void cacheConfig(int cs, int bs, int assoc, char *trace){
+void cacheConfig(int cs, int bs, int assoc, int nbe, char *trace){
     puts("Cache configuration :");
-    puts(" _______________________________________ ");
-    puts("|\tVariable\t\t\t|\tValue\t\t|");
-    puts("|-----------------------|---------------|");
-    printf("|\tCache size\t\t\t|\t%d\t\t\t|\n", cs);
-    printf("|\tCache bloc size\t\t|\t%d\t\t|\n", bs);
-    printf("|\tCache associativity\t|\t%d\t\t\t|\n", assoc);
-    printf("|\tTrace\t\t\t\t|\t%s\t|\n", trace);
-    puts("|_______________________|_______________|");
+    printf("Cache size\t\t\t\t%d\n", cs);
+    printf("Cache bloc size\t\t\t%d\n", bs);
+    printf("Cache associativity\t\t%d\n", assoc);
+    printf("Set number\t\t\t\t%d\n", nbe);
+    printf("Trace\t\t\t\t\t%s\n", trace);
+    puts("");
+    puts("");
 }
 
 
 int main(int argc, char *argv[]) {
 
     char *tmp;
+    int nbr_r = 0, nbr_w = 0;
 
     int cs = (int)strtol(argv[1], &tmp, 10);
     int bs = (int)strtol(argv[2], &tmp, 10);
     int assoc = (int) strtol(argv[3], &tmp, 10);
-    char *trace = argv[4];
     int nbe = cs/(bs*assoc);
+    char *trace = argv[4];
 
     bloc cache[nbe][assoc];
 
     initCache(nbe, assoc, cache);
-    cacheConfig(cs, bs, assoc, trace);
+    cacheConfig(cs, bs, assoc, nbe, trace);
 
+    char car, *adr, i = 0;
 
+    FILE *f = fopen("test.txt", "r");
+    if(f != NULL){
+        while(!feof(f)){
+            fscanf(f, "%c%s\n", &car, adr);
+            printf("Ligne %d : %c est un caractère.\n%s est une adresse.\n\n", i, car, adr);
+            i++;
+        }
+    fclose(f);
+    }
 }
