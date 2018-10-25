437ce4ab57448732c88cc848e0838e904ee810cf
Unknown <anthony.marechal@yahoo.fr>
2018-10-25 13:57:29 +0200

    :tada: New start
    Chained lists, nru functions, queue functions, ...

--- 154.c
+++ 154.c
@@ -5,22 +5,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 
-typedef struct block {
-    int valid;
-    double tag;
-} bloc;
+typedef struct Block {
+    short nruB;
+    unsigned long int tag;
+    struct Block *next;
+} Block;
 
-int nbr_r = 0, nbr_w = 0;
+typedef struct Set {
+    int content;
+    struct Block *head;
+    struct Block *queue;
+} Set;
 
-// Cache initializer
-void initCache(long nbe, long assoc, bloc cache[nbe][assoc]) {
-    int i, j;
-    for (i = 0; i < nbe; i++)
-        for (j = 0; j < assoc; j++)
-            cache[i][j].valid = 0;
-}
+int sets = 0, bs = 0, cs = 0, blockNum = 1;
+
+int nbr_r = 0, nbr_w = 0, hits = 0, misses = 0;
 
 // Print out cache configuration
 void cacheConfig(int cs, int bs, int assoc, int nbe, char *trace) {
@@ -34,24 +36,185 @@ void cacheConfig(int cs, int bs, int assoc, int nbe, char *trace) {
     puts("");
 }
 
-// Read & Write counter
-void RW_counter(char car) {
-    if (car == 'R')
-        nbr_r++;
-    else if (car == 'W')
-        nbr_w++;
+// Print out cache content
+void printCache(Set *cache){
+    int i;
+    for(i = 0; i < sets; i++){
+        struct Block *tmp = cache[i].head;
+        while(tmp != NULL){
+            printf("%lu : ", tmp->tag);
+            tmp = tmp->next;
+        }
+        puts("");
+    }
+    printf("\t\t| END");
 }
 
-// Convert into hexa (int) a string (adress of the file)
-int stringToHexa(char *adr) {
-    return (int) strtol(adr, NULL, 16);
+void mapping(Set *cache, unsigned long int tagBVal, int setIndex, int swap){
+    if((cache[setIndex].head != NULL) &&
+            (cache[setIndex].head->tag == tagBVal)){
+        if(swap == 0)
+            hits++;
+        else{
+            hits++;
+            nbr_w++;
+        }
+    } else {
+        if(swap == 0) {
+            misses++;
+            nbr_r++;
+        } else {
+            misses++;
+            nbr_r++;
+            nbr_w++;
+        }
+        cache[setIndex].head = (struct Block *)(malloc(sizeof(struct Block)));
+        cache[setIndex].head->tag = tagBVal;
+    }
 }
 
-void isReadWrite(char car){
-    if (car == 'R')
-        nbr_r++;
-    else if (car == 'W')
-        nbr_w++;
+void enqueue(Set *cache, int index, unsigned long int tag, int swap){
+    struct Block *tmp = cache[index].queue;
+    cache[index].queue = (struct Block *)(malloc(sizeof(struct Block)));
+    cache[index].queue->tag = tag;
+    cache[index].queue->next = NULL;
+
+    if(cache[index].head == NULL){
+        cache[index].head = cache[index].queue;
+        if(swap == 0){
+            misses++;
+            nbr_r++;
+        } else {
+            misses++;
+            nbr_r++;
+            nbr_w++;
+        }
+    } else{
+        if(swap == 0){
+            misses++;
+            nbr_r++;
+        } else {
+            misses++;
+            nbr_r++;
+            nbr_w++;
+        }
+        tmp->next = cache[index].queue;
+    }
+
+    if (cache[index].content < blockNum){
+        cache[index].content = cache[index].content + 1;
+    }
+}
+
+void dequeue(Set *cache, int index, unsigned long int tag, int swap){
+    struct Block *tmp = cache[index].head;
+
+    if(cache[index].head != NULL){
+        cache[index].head = cache[index].head->next;
+        free(tmp);
+    }
+
+    dequeue(cache, index, tag, swap);
+}
+
+void addToHead(Set *cache, int setIndex, unsigned long int tag){
+    struct Block *node = (struct Block *)(malloc(sizeof(struct Block)));
+    node->tag = tag;
+    node->next = NULL;
+
+    if(cache[setIndex].head == NULL){
+        cache[setIndex].head = node;
+    } else {
+        struct Block *tmp = cache[setIndex].head;
+        cache[setIndex].head = node;
+        node->next = tmp;
+    }
+
+    cache[setIndex].content = cache[setIndex].content + 1;
+}
+
+void addToEnd(Set *cache, int setIndex, unsigned long int tag){
+    struct Block *node = (struct Block *)(malloc(sizeof(struct Block)));
+    node->tag = tag;
+    node->next = NULL;
+    node->nruB = 0;
+
+    if(cache[setIndex].head == NULL){
+        cache[setIndex].head = node;
+    } else {
+        struct Block *tmp = cache[setIndex].head;
+
+        while(tmp != NULL){
+            if(tmp->next == NULL){
+                tmp->next = node;
+            } else {
+                tmp = tmp -> next;
+            }
+        }
+    }
+
+    cache[setIndex].content = cache[setIndex].content + 1;
+}
+
+void newNRU(Set *cache, unsigned long int tagBVal, int setIndex){
+    struct Block *tmp = cache[setIndex].head;
+    int found = 0;
+
+    while (tmp != NULL){
+        if(tmp->nruB == 1){
+            tmp->nruB = 0;
+            tmp->tag = tagBVal;
+            found = 1;
+        } else
+            tmp = tmp->next;
+    }
+
+    struct Block *new = cache[setIndex].head;
+
+    if(found == 0){
+        while ((new != NULL) && (new->nruB == 0)){
+            new->nruB = 1;
+            new = new->next;
+        }
+        newNRU(cache, tagBVal, setIndex);
+    }
+}
+
+void nru(Set *cache, unsigned long int tagBVal, int setIndex, int swap) {
+    struct Block *tmp = cache[setIndex].head;
+    int found = 0;
+
+    while(tmp != NULL){
+        if(tmp->tag == tagBVal){
+            tmp->nruB = 0;
+            if(swap == 0)
+                hits++;
+            else{
+                hits++;
+                nbr_w++;
+            }
+            found = 1;
+        } else
+            tmp = tmp->next;
+    }
+
+    if(found == 0){
+
+        if(swap == 0){
+            misses++;
+            nbr_r++;
+        } else {
+            misses++;
+            nbr_r++;
+            nbr_w++;
+        }
+
+        if(cache[setIndex].content < blockNum) {
+            addToEnd(cache, setIndex, tagBVal);
+        } else {
+            newNRU(cache, tagBVal, setIndex);
+        }
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -64,23 +227,5 @@ int main(int argc, char *argv[]) {
     int nbe = cs/(bs*assoc);
     char *trace = argv[4];
 
-    bloc cache[nbe][assoc];
 
-    initCache(nbe, assoc, cache);
-    cacheConfig(cs, bs, assoc, nbe, trace);
-
-    char car, *adr, i = 0;
-
-    FILE *f = fopen("test.txt", "r");
-    if(f != NULL){
-        while(!feof(f)){
-            fscanf(f, "%c%s\n", &car, adr);
-            printf("Ligne %d : %c est un caractère.\n%s est une adresse.\n\n", i, car, adr);
-            isReadWrite(car);
-            i++;
-        }
-        fclose(f);
-    }
-    printf("%d écritures.\n"
-           "%d lectures.\n", nbr_w, nbr_r);
 }
